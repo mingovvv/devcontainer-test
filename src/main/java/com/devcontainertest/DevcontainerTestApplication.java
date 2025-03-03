e4e383d13078f97b12e984c3b657e375a1907240
mingovvv <mingovvv@gmail.com>
2025-03-03 18:12:48 +0000

    test

---
+++ src/main/java/com/devcontainertest/DevcontainerTestApplication.java
@@ -0,0 +1,13 @@
+package com.devcontainertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcontainerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevcontainerTestApplication.class, args);
+    }
+
+}
